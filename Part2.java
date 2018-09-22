d808ef0111ba27db4cce840ce6b1be0724b2e1a0
G_Ivo <gregorybivo@gmail.com>
2018-09-22 15:00:47 -0400

    The is a mess, but it works!

--- Part2.java
+++ Part2.java
@@ -1,3 +1,4 @@
+package lab1;
 import java.io.*;
 import java.util.*; 
 
@@ -8,18 +9,23 @@ public class Part2 {
 		System.out.println("Testing size of: " + size);
 		for(int x = 0; x < 5; x++) {
 			//To Test Simple array
+			
 			long startTime = System.nanoTime();
 			int sum1 = 0;
 			int[] test1 = new int[size];
 			for(int i = 0; i < size; i++) {
 				test1[i] = (int) (Math.random()*((9-0+1))+0);
 			}
+			long endTime = System.nanoTime();
+			long durationFirst = (endTime - startTime)/1000000;
+			startTime = System.nanoTime();
+			
 			for(int i = 0; i < size; i++) {
 				sum1 += test1[i];
 			}
-			long endTime = System.nanoTime();
-			long duration = (endTime - startTime);  //divide by 1000000 to get milliseconds.
-			System.out.println("Test# "+x+"\nBasic Array: \n\t Duration: " + (duration/1000000) + " millisecconds, Sum: " + sum1);
+			endTime = System.nanoTime();
+			long durationSeccond = (endTime - startTime)/1000000;  //divide by 1000000 to get milliseconds.
+			System.out.println("Test# "+x+"\nBasic Array: \n\t Duration to create: " + (durationFirst) + " millisecconds, Duration to Sum: " + durationSeccond + " millisecconds, total Sum: "+ sum1 + "\n\t\t Total Time: " + (durationFirst + durationSeccond) + " millisecconds");
 			
 			//to Test ArrayList
 			startTime = System.nanoTime();
@@ -28,12 +34,16 @@ public class Part2 {
 			for(int i = 0; i < size; i++) {
 				test2.add((int) (Math.random()*((9-0+1))+0));
 			}
+			endTime = System.nanoTime();
+			durationFirst = (endTime - startTime)/1000000;
+			startTime = System.nanoTime();
+			
 			for(int i = 0; i < size; i++) {
 				sum2 += test2.get(i);
 			}
 			endTime = System.nanoTime();
-			duration = (endTime - startTime);  //divide by 1000000 to get milliseconds.
-			System.out.println("ListArray: \n\t Duration: " + (duration/1000000) + " millisecconds, Sum: " + sum2);
+			durationSeccond = (endTime - startTime)/1000000;  //divide by 1000000 to get milliseconds.
+			System.out.println("ArrayList: \n\t Duration to create: " + (durationFirst) + " millisecconds, Duration to Sum: " + durationSeccond + " millisecconds, total Sum: "+ sum1 + "\n\t\t Total Time: " + (durationFirst + durationSeccond) + " millisecconds");
 			
 			//To Test VECTOR
 			startTime = System.nanoTime();
@@ -42,12 +52,15 @@ public class Part2 {
 			for(int i = 0; i < size; i++) {
 				test3.add((int) (Math.random()*((9-0+1))+0));
 			}
+			endTime = System.nanoTime();
+			durationFirst = (endTime - startTime)/1000000;
+			startTime = System.nanoTime();
 			for(int i = 0; i < size; i++) {
 				sum3 += test3.get(i);
 			}
 			endTime = System.nanoTime();
-			duration = (endTime - startTime);  //divide by 1000000 to get milliseconds.
-			System.out.println("Vector: \n\t Duration: " + (duration/1000000) + " millisecconds, Sum: " + sum3);
+			durationSeccond = (endTime - startTime)/1000000;  //divide by 1000000 to get milliseconds.
+			System.out.println("Vector: \n\t Duration to create: " + (durationFirst) + " millisecconds, Duration to Sum: " + durationSeccond + " millisecconds, total Sum: "+ sum1 + "\n\t\t Total Time: " + (durationFirst + durationSeccond) + "millisecconds");
 		}
 	}
 	
